f84e2018c03eeec15cfc3a05367c06679acaa0d0
Kerbastard Clément <clement@opti-it.fr>
2018-03-07 15:50:14 +0100

    ping.bat
    Test ping for network

--- ping.bat
+++ ping.bat
@@ -3,27 +3,27 @@
 ::            FILE: ping.bat
 ::         VERSION: 1.3
 ::           USAGE: Lancer ping.bat autant de fois qu'il y a d'�quipement � tester,
-::                  des logs vont se cr�er � la racine du script
-::     DESCRIPTION: Permet de tester le r�seau d'un client en pinguant depuis le
+::                  des logs vont se créer à la racine du script
+::     DESCRIPTION: Permet de tester le réseau d'un client en pinguant depuis le
 ::                  serveur vers les postes clients, vers les �quipements r�seaux,
 ::                  vers internet
 ::         OPTIONS: -
 ::    REQUIREMENTS: -
-::            BUGS: Parfois la fen�tre n'affiche pas les commentaires mais le script tourne
-::           NOTES: Fonctionne avec ping.exe en anglais ou en fran�ais. A savoir,
-::                  dans la version fran�aise du ping.exe, l'affichage du r�sultat
-::                  est diff�rent quand ms est sup�rieur ou �gal � 1
-::          AUTHOR: KERBASTARD Cl�ment
+::            BUGS: Parfois la fenêtre n'affiche pas les commentaires mais le script tourne
+::           NOTES: Fonctionne avec ping.exe en anglais ou en français. A savoir,
+::                  dans la version française du ping.exe, l'affichage du résultat
+::                  est différent quand ms est supérieur ou égal à 1
+::          AUTHOR: KERBASTARD Clément
 ::         COMPANY: FUTUROSOFT
 ::         CREATED: 01/06/17
 ::        REVISION: 06/03/17
 ::==============================================================================
 
-:: D�claration des variables IP et MS, � saisir � l'ex�cution du script
+:: Déclaration des variables IP et MS, à saisir à l'exécution du script
 set /p IP="Saisir une IP : "
 set /p MS="Valeur de test (en ms) : "
 
-:: D�claration des variables
+:: Déclaration des variables
 set LOG="log_%COMPUTERNAME%_%IP%.txt"
 set HEURE=%time:~,8%
 set JOUR=%date%
@@ -31,7 +31,7 @@ set CHECK=1800
 
 cls
 
-:: Bloque de commentaire dans un fichier log et affichage � l'�cran
+:: Bloque de commentaire dans un fichier log et affichage à l'écran
 (echo Test Ping en cours
 echo Commencer le %JOUR%, %HEURE%
 echo De %COMPUTERNAME% vers %IP%
@@ -43,14 +43,14 @@ echo ===============================
 echo.) >> %LOG% | type %LOG%
 
 :: Boucle relancer x fois (CHECK) permettant de logguer une ligne
-:: Si le script s'arr�te, cela permet de savoir quand
+:: Si le script s'arrète, cela permet de savoir quand
 :loop
 set /a i=0
 echo "====== %JOUR% %HEURE% ======" >> %LOG%
 
 :: Commande ping avec 1 paquet et 1000ms (1s) d'attente
-:: Si d�lai d�pass� ou h�te introuvable alors "Ne repond pas" dans le log
-:: Si dans le d�lai mais le r�sultat sup�rieur ou �gal � la valeur de test en ms alors log
+:: Si délai dépassé ou hôte introuvable alors "Ne repond pas" dans le log
+:: Si dans le délai mais le résultat supérieur ou égal à la valeur de test en ms alors log
 :: La mini-boucle se relance toutes les secondes
 :miniloop
 set /a i=i+1
